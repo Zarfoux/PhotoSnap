4bae26fd405d186c02acf6593b4e816e26446c22
Syfux <sylvain.carrier@orange.fr>
2019-03-15 14:26:06 +0100

    Image

--- app/src/main/java/helloandroid/m2dl/photosnap/MainActivity.java
+++ app/src/main/java/helloandroid/m2dl/photosnap/MainActivity.java
@@ -2,9 +2,12 @@ package helloandroid.m2dl.photosnap;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import helloandroid.m2dl.photosnap.domain.Ball;
@@ -19,6 +22,15 @@ public class MainActivity extends AppCompatActivity {
 
     private static final int REQUEST_IMAGE_CAPTURE = 1;
     private ImageView imageView;
+    private Button button;
+    private Bitmap bitmap;
+
+    private View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            dispatchTakePictureIntent();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,9 +48,9 @@ public class MainActivity extends AppCompatActivity {
         gameView.setGameContext(gameContext);
         gameView.invalidate();
 
-        //imageView = findViewById(R.id.imageView);
-
-        dispatchTakePictureIntent();
+        imageView = findViewById(R.id.imageView);
+        button = findViewById(R.id.button);
+        button.setOnClickListener(onClickListener);
     }
 
     private void dispatchTakePictureIntent() {
@@ -53,7 +65,39 @@ public class MainActivity extends AppCompatActivity {
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
             Bundle extras = data.getExtras();
             Bitmap imageBitmap = (Bitmap) extras.get("data");
+            bitmap = convertBitmap(imageBitmap);
             imageView.setImageBitmap(imageBitmap);
         }
+    }
+
+    public static Bitmap convertBitmap(Bitmap src){
+        int width = src.getWidth();
+        int height = src.getHeight();
+        // create output bitmap
+        Bitmap bmOut = Bitmap.createBitmap(width, height, src.getConfig());
+        // color information
+        int A, R, G, B;
+        int pixel;
+        for (int x = 0; x < width; ++x) {
+            for (int y = 0; y < height; ++y) {
+                // get pixel color
+                pixel = src.getPixel(x, y);
+                A = Color.alpha(pixel);
+                R = Color.red(pixel);
+                G = Color.green(pixel);
+                B = Color.blue(pixel);
+                int gray = (int) (0.2989 * R + 0.5870 * G + 0.1140 * B);
+                // use 128 as threshold, above -> white, below -> black
+                if (gray > 115) {
+                    gray = 255;
+                }
+                else{
+                    gray = 0;
+                }
+                // set new pixel color to output bitmap
+                bmOut.setPixel(x, y, Color.argb(A, gray, gray, gray));
+            }
+        }
+        return bmOut;
     }
 }
